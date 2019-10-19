afecb0e9b0e4ae69bab946b13373333f38943228
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-10-19 13:35:17 -0500

    wip

--- driver.js
+++ driver.js
@@ -3,5 +3,5 @@ var serveStatic = require('serve-static')
  
 var app = express()
  
-app.use(serveStatic('public/ftp', { 'index': ['resume.html'] }))
+app.use(serveStatic('assets', { 'index': ['resume.html'] }))
 app.listen(process.env.PORT || 3000)
